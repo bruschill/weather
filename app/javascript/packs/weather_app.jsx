9d96616a34850b33e9deb997a4817ac5f646c200
B. Ruschill <brandon.ruschill@pm.me>
2024-04-06 18:19:34 -0500

    add basic components, install google places input

--- app/javascript/packs/weather_app.jsx
+++ app/javascript/packs/weather_app.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+
 import "./weather_app.css";
 
 // const GOOGLE_PLACES_API_KEY = process.env.GOOGLE_PLACES_API_KEY
@@ -10,12 +12,43 @@ const WeatherApp = () => {
       <div></div>
 
       <div>
-        <h1>Weather</h1>
+        <Header/>
+        <AddressInput/>
+        <CurrentWeather/>
+        <FiveDayForecast/>
       </div>
 
       <div></div>
     </div>
   );
+}
+
+const Header = () => {
+  return (
+    <h1>Weather</h1>
+  )
+}
+
+const AddressInput = () => {
+  return (
+    <div>
+      <GooglePlacesAutocomplete
+        apiKey="****"
+      />
+    </div>
+  )
+}
+
+const CurrentWeather = () => {
+  return (
+    <h1>Current Weather</h1>
+  )
+}
+
+const FiveDayForecast = () => {
+  return (
+    <h1>Five-day Forecast</h1>
+  )
 }
 
 document.addEventListener('DOMContentLoaded', () => {
