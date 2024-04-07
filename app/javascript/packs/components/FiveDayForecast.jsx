acf65847b75bdb900e8d7b99837f0137600cdffd
B. Ruschill <brandon.ruschill@pm.me>
2024-04-07 16:00:26 -0500

    hook up forecast data

--- app/javascript/packs/components/FiveDayForecast.jsx
+++ app/javascript/packs/components/FiveDayForecast.jsx
@@ -7,9 +7,20 @@ import { Header } from "./Header";
 export const FiveDayForecast = () => {
   const [state] = useAppState();
   const forecastData = state["forecast"]
-  // console.log(forecastData);
 
   return (
-    <Header text="Five-day Forecast"/>
+    <>
+      <Header text="Five-day Forecast"/>
+      {
+        Object.entries(forecastData).map(([key, value]) => {
+          return (
+            <div>
+              {value["day_name"]} -> H:{value["fahrenheit"]["temp_max"]}° L:{value["fahrenheit"]["temp_min"]}°
+            </div>
+          )
+        })
+      }
+
+    </>
   )
 }
