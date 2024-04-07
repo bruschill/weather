7e489738ba73e7f0dacf64858462cd4b3c702d32
B. Ruschill <brandon.ruschill@pm.me>
2024-04-07 17:30:23 -0500

    round temperatures to nearest whole number

--- app/lib/open_weather_map_api.rb
+++ app/lib/open_weather_map_api.rb
@@ -107,7 +107,12 @@ class OpenWeatherMapAPI
   def parse_current_weather_response_data(data)
     location_name = data["location"]
     environment_data = data["main"].select { |key, _value| key.in?(%W[humidity pressure]) }
-    fahrenheit_temperature_data = data["main"].select { |key, _value| key.in?(%W[temp temp_min temp_max feels_like]) }
+    fahrenheit_temperature_data = data["main"].each_with_object({}) do |(key, value), hash|
+      if key.in?(%W[temp temp_min temp_max feels_like])
+        hash[key] = value.round
+      end
+    end
+
     celsius_temperature_data = convert_fahrenheit_temperature_data_to_celsius(fahrenheit_temperature_data)
     wind_data = data["wind"]
     current_conditions_data = data["weather"][0]["main"]
@@ -127,23 +132,22 @@ class OpenWeatherMapAPI
   end
 
   def parse_forecast_response_data(data)
-    location_name = data["city"]["name"]
     five_day_forecast = data["list"]
 
     preprocessed_five_day_forecast = five_day_forecast.each_with_object(Hash.new { |hash, key| hash[key] = [] }) do |forecast_interval, hash|
       # iterating by interval
       date = Date.parse(forecast_interval["dt_txt"])
       min_max_temp_hash = {
-        temp_min: forecast_interval["main"]["temp_min"],
-        temp_max: forecast_interval["main"]["temp_max"]
+        temp_min: forecast_interval.dig("main", "temp_min"),
+        temp_max: forecast_interval.dig("main", "temp_max")
       }
 
       hash[date.to_s] << min_max_temp_hash
     end
 
     preprocessed_five_day_forecast.each_with_object({}) do |(key, value), hash|
-      min_temp = value.map { |v| v[:temp_min] }.min
-      max_temp = value.map { |v| v[:temp_max] }.max
+      min_temp = value.map { |v| v[:temp_min] }.min.round
+      max_temp = value.map { |v| v[:temp_max] }.max.round
       date = Date.parse(key)
 
       day_name = if date.today?
