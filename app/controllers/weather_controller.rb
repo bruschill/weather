d770d750d7e3e660c3d9e425367b4781389666cd
B. Ruschill <brandon.ruschill@pm.me>
2024-04-07 11:50:34 -0500

    heavy-handedly parse 5-digit postal code out of whatever the user sends

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -3,8 +3,20 @@ class WeatherController < ApplicationController
     owm = OpenWeatherMapAPI.new
 
     # parse zip out of params[:q] to the best of my ability
-    data = owm.weather(params[:q])
+    parsed_address = StreetAddress::US.parse(params[:q])
+    postal_code = parsed_address&.postal_code
 
-    render json: data
+    data = if postal_code.present?
+             owm.weather(postal_code)
+           else
+             # \b(\d{5})\b: Matches a ZIP code consisting of five digits with word boundaries.
+             # (?!.*\b\d{5}\b): Negative lookahead assertion to ensure that there are no more occurrences of a ZIP code pattern (\b\d{5}\b) ahead in the string.
+             five_digit_postal_code_regex = /\b(\d{5})\b(?!.*\b\d{5}\b)/
+
+             postal_code = five_digit_postal_code_regex.match(params[:q])[0]
+             owm.weather(postal_code)
+           end
+
+   render json: data
   end
 end
