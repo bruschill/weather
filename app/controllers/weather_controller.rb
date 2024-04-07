e928c3f7355ffc276ab96187978ff0ffce8f85cc
B. Ruschill <brandon.ruschill@pm.me>
2024-04-07 11:56:28 -0500

    "safer" handling of postal code parsing

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -4,19 +4,23 @@ class WeatherController < ApplicationController
 
     # parse zip out of params[:q] to the best of my ability
     parsed_address = StreetAddress::US.parse(params[:q])
-    postal_code = parsed_address&.postal_code
 
-    data = if postal_code.present?
-             owm.weather(postal_code)
-           else
-             # \b(\d{5})\b: Matches a ZIP code consisting of five digits with word boundaries.
-             # (?!.*\b\d{5}\b): Negative lookahead assertion to ensure that there are no more occurrences of a ZIP code pattern (\b\d{5}\b) ahead in the string.
-             five_digit_postal_code_regex = /\b(\d{5})\b(?!.*\b\d{5}\b)/
+    postal_code = if parsed_address.present?
+                    parsed_address.postal_code
+                  else
+                    # \b(\d{5})\b: Matches a ZIP code consisting of five digits with word boundaries.
+                    # (?!.*\b\d{5}\b): Negative lookahead assertion to ensure that there are no more occurrences of a ZIP code pattern (\b\d{5}\b) ahead in the string.
+                    five_digit_postal_code_regex = /\b(\d{5})\b(?!.*\b\d{5}\b)/
 
-             postal_code = five_digit_postal_code_regex.match(params[:q])[0]
-             owm.weather(postal_code)
-           end
+                    five_digit_postal_code_regex.match(params[:q])[0]
+                  end
 
-   render json: data
+    if postal_code.present?
+      weather_data = owm.weather(postal_code)
+
+      render json: weather_data
+    else
+      render json: { errors: ["something messed up"]}
+    end
   end
 end
